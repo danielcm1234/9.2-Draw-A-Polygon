c4beae0cbdb1485ea9000c8247b4904c11798077
danielcm1234 <danielcookemazur@gmail.com>
2026-04-13 16:24:42 -0500

    not done

--- main.py
+++ main.py
@@ -27,6 +27,18 @@ while True:
         pen.write("polygons must have at least 3 sides.")
     elif sides!= 4:
         regular_polygon(turtle, sides)
+    elif sides == 4:
+        parralel_sides = int(input("How many sides are parralell?"))
+        if parralel_sides == 0:
+            turtle.begin_fill()
+            turtle.goto(100, 200)
+            turtle.goto(50, -326)
+            turtle.goto(-75, -300)
+            #turtle.goto(-100, 310)
+            turtle.end_fill()
+            turtle.home()
+        elif parralel_sides == 2:
+            
 
 
 
